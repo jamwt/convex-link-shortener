ed2bff33f103742f35932fb02a33ede24dfb0041
Jamie Turner <jamwt@convex.dev>
2024-08-13 19:21:22 -0700

    Update schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,8 +3,12 @@ import { v } from "convex/values";
 
 export default defineSchema({
   links: defineTable({
+    // Alternative domain name. Empty string if you're just using a path-based match.
     domain: v.string(),
+    // Target for the redirection. The "long" link.
     full: v.string(),
-    short: v.string(),
+    // The path fragment after your short link domain. In `link.me/ab123`, it's `ab123`.
+    // If you're matching on a domain name, put empty string.
+    short: v.string(), 
   }).index("by_domain", ["domain", "short"]),
 });
